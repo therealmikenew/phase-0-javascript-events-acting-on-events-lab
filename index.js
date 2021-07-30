49e3ee3604f2b1c52f93bda2e2efbc5aabe98e1b
Michael Newman <therealmichaelnewman@gmail.com>
2021-07-30 16:43:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = '#7FFF00';
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    if (left > 0) {
+        dodger.style.left = `${left -1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft ();
+    } else if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+    
+});
+
+
+
+    
+
+
